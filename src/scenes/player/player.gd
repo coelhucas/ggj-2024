13e73e99c6b4ed60b555e3bcc6f5ab1708db6dc2
Lucas Coelho <lucascoelhodacosta@gmail.com>
2024-01-26 23:05:56 -0300

    fix jump

--- src/scenes/player/player.gd
+++ src/scenes/player/player.gd
@@ -2,14 +2,14 @@ extends CharacterBody2D
 
 const GRAVITY := 50
 const SPEED := 400
-const JUMP_FORCE := -800
+const JUMP_FORCE := -600
 
 func _physics_process(delta):
 	var _input := Input.get_axis("p1_left", "p1_right")
 	velocity.x = _input * SPEED
 	velocity.y += GRAVITY
 	
-	if Input.is_action_just_pressed("p1_up"):
+	if Input.is_action_just_pressed("p1_up") and is_on_floor():
 		velocity.y = JUMP_FORCE
 	
 	move_and_slide()
