b675f7bea7619253ee12c67420bec3dfb5a6fc54
Lorran Sutter <sutterlorran@gmail.com>
2024-01-27 15:22:21 -0300

    audience | slime | spike ball | hero

--- src/scenes/public/person.gd
+++ src/scenes/public/person.gd
@@ -1,28 +1,32 @@
 extends Node2D
 
-@onready var sprite := $Sprite2D
+#@onready var sprite := $Sprite2D
 @onready var skin := $Skin
 
 var offset := 32
 var player: Node2D
 
-
 func _ready() -> void:
-	skin.frame = randi() % skin.hframes
+	var hFrameIndex = 1 + randi() % ((skin.hframes - 2)/3) * 3
+	var vFrameIndex = randi() % skin.vframes
+	if vFrameIndex % 4 != 0:
+		vFrameIndex += 4 - (vFrameIndex % 4)
+	
+	skin.frame = vFrameIndex * skin.hframes + hFrameIndex
 	Global.set_player.connect(func(_p):
 		player = _p
 	)
 
 
-func _process(delta):
-	if not is_instance_valid(player):
-		return
-	
-	if player.global_position.x > global_position.x and abs(player.global_position.x - global_position.x) > offset:
-		sprite.frame = 3
-	elif player.global_position.x < global_position.x  and abs(player.global_position.x - global_position.x) > offset:
-		sprite.frame = 2
-	
-	if abs(player.global_position.x - global_position.x) < offset:
-		sprite.frame = 0
+#func _process(delta):
+	#if not is_instance_valid(player):
+		#return
+	#
+	#if player.global_position.x > global_position.x and abs(player.global_position.x - global_position.x) > offset:
+		#sprite.frame = 3
+	#elif player.global_position.x < global_position.x  and abs(player.global_position.x - global_position.x) > offset:
+		#sprite.frame = 2
+	#
+	#if abs(player.global_position.x - global_position.x) < offset:
+		#sprite.frame = 0
 	
