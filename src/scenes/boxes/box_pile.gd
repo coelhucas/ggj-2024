0452a65cab0ca5ad0275478df271971291b5f35a
Lucas Coelho <lucascoelhodacosta@gmail.com>
2024-01-27 22:18:34 -0300

    bricks sfx

--- src/scenes/boxes/box_pile.gd
+++ src/scenes/boxes/box_pile.gd
@@ -1,12 +1,16 @@
 extends Area2D
 
+@onready var sfx_pick := $AudioStreamPlayer
+
 signal took_box
 
 var visible_boxes = Global.MAX_BOXES
 
 func _on_body_entered(body):	
 	if body == %Player:
-		if not body.box_icon.visible:
+		if not body.box_icon.visible and visible_boxes > 0:
+			sfx_pick.pitch_scale = randf_range(0.8, 1.2)
+			sfx_pick.play()
 			get_child(visible_boxes - 1).visible = false
 			visible_boxes -= 1
 			took_box.emit()
--- src/scenes/boxes/box_stairs.gd
+++ src/scenes/boxes/box_stairs.gd
@@ -1,17 +1,21 @@
 extends Area2D
 
+@onready var sfx_drop := $AudioStreamPlayer
+
 signal placed_box
 
 var visible_boxes = 0
 
 func _ready():
 	for child in get_children():
-		if child != $CollisionShape2D:
+		if child != $CollisionShape2D and not child is AudioStreamPlayer:
 			child.visible = false
 
 func _on_body_entered(body):
 	if body == %Player:
 		if body.box_icon.visible:
+			sfx_drop.pitch_scale = randf_range(0.8, 1.2)
+			sfx_drop.play()
 			placed_box.emit()
 			get_child(visible_boxes).visible = true
 			visible_boxes += 1
