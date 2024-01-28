cf7547dbbb3cf0a81d2907ffb99cdf4bc7768546
Lucas Coelho <lucascoelhodacosta@gmail.com>
2024-01-28 16:09:14 -0300

    new game over screen

--- src/scenes/ui/ui.gd
+++ src/scenes/ui/ui.gd
@@ -1,11 +1,15 @@
 extends CanvasLayer
 
 @onready var end_game_panel := $EndGame
-@onready var end_game_label := $EndGame/PanelContainer/MarginContainer/VBoxContainer/VictoryLabel
-@onready var btn_restart := $EndGame/PanelContainer/MarginContainer/VBoxContainer/BtnRestart
+@onready var btn_restart := $EndGame/BtnRestart
+@onready var end_game_bg := $EndGame/Bg
+
+@export var texture_king_win: Texture2D
+@export var texture_jester_win: Texture2D
 
 
 func _ready() -> void:
+	end_game_panel.hide()
 	Global.game_over.connect(display)
 	btn_restart.pressed.connect(get_tree().reload_current_scene)
 
@@ -14,5 +18,5 @@ func display(_king_win: bool) -> void:
 	if not _king_win:
 		Global.play_win_sfx()
 
-	end_game_label.text = "King wins!" if _king_win else "Jester wins!"
+	end_game_bg.texture = texture_king_win if _king_win else texture_jester_win
 	end_game_panel.show()
